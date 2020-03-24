c4eea51578a157e9db1017096073a4d400abdc41
FelixMusic <62540334+FelixMusic@users.noreply.github.com>
2020-03-24 14:55:04 +0300

    Add files via upload

--- parser_wildberries_postelnoe.py
+++ parser_wildberries_postelnoe.py
@@ -3,6 +3,7 @@ import requests
 from fake_useragent import UserAgent
 import xlwt
 import time
+import datetime
 import lxml
 import random
 
@@ -15,7 +16,7 @@ price = []
 price_discount = []
 brand = []
 
-for i in range(1, 20): # 19 страниц
+for i in range(1, 2): # 19 страниц
     url = 'https://www.wildberries.ru/catalog/0/search.aspx?subject=' \
           '883&search=%D0%BF%D0%BE%D1%81%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%' \
           'D0%BE%D0%B5%20%D0%B1%D0%B5%D0%BB%D1%8C%D0%B5&page=' + str(i)
@@ -74,6 +75,13 @@ for link in links:
         price_discount.append(0)
     j = j + 1
 
+# имя файла xls с текущей датой
+date_now = datetime.datetime.now()
+date_now_list = list(map(str, str(date_now).split()))
+now_list = list(map(str, str(date_now_list[0]).split('-')))
+datefld = str(now_list[2]) + '.' + str(now_list[1]) + '.' + str(now_list[0])
+xls_file_name = 'WB_postelnoe_' + datefld + '.xls'
+
 # Далее зиписываем данные в файл .xls
 book = xlwt.Workbook('utf8')  # Создаем книгу
 # Создаем шрифт
@@ -81,7 +89,7 @@ font = xlwt.easyxf('font: height 240,name Arial,colour_index black, bold off,\
     italic off; align: wrap on, vert top, horiz left;\
     pattern: pattern solid, fore_colour white;')
 # Добавляем лист
-sheet = book.add_sheet('wildberries постельное белье')
+sheet = book.add_sheet('WB постельное')
 # Заполняем ячейки (Строка, Колонка, Текст, Шрифт)
 m = 0
 for i in range(len(links)):
@@ -103,4 +111,4 @@ sheet.col(5).width = 26000
 
 sheet.portrait = False  # Лист в положении "альбом"
 sheet.set_print_scaling(85)  # Масштабирование при печати
-book.save('wildberries_postelnoe.xls')  # Сохраняем в файл
+book.save(xls_file_name)  # Сохраняем в файл
