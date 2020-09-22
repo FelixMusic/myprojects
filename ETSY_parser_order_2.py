3f2d1ec4583503c9e91ec0a158c52f48ec8bdd78
FelixMusic <62540334+FelixMusic@users.noreply.github.com>
2020-09-22 15:31:40 +0300

    Add files via upload

---
+++ ETSY_parser_order_2.py
@@ -0,0 +1,78 @@
+﻿from bs4 import BeautifulSoup
+import requests
+from fake_useragent import UserAgent
+import xlwt
+import time
+import random
+import socks
+import socket
+
+# работаем под запущенным Tor-браузером
+socks.set_default_proxy(socks.SOCKS5, "localhost", 9150)
+socket.socket = socks.socksocket
+
+useragent = UserAgent()
+
+names = []
+links = []
+images = []
+
+for i in range(1, 217): # 216 страниц
+    url = 'https://www.etsy.com/shop/NikkiPattern/reviews?ref=pagination&page=' + str(i)
+    response = requests.get(url, headers={'User-Agent': useragent.random})  # подменяем агент
+    print(response.status_code, i)  # статус сервера (должен быть 200)
+    # если статус не 200, делаем паузу 60-80 сек и снова делаем запрос:
+    if response.status_code != 200:
+        time.sleep(random.randrange(60, 80, 1))
+        url = 'https://www.etsy.com/shop/NikkiPattern/reviews?ref=pagination&page=' + str(i)
+        response = requests.get(url, headers={'User-Agent': useragent.random})
+        print('  ', response.status_code, i)
+
+    html = response.content
+    soup = BeautifulSoup(html, 'html.parser')
+
+    names_soup = soup.find_all('div', class_='flag-body hide-xs hide-sm')
+    for name in names_soup:
+        name = name.find('p').text
+        names.append(name)
+
+    links_soup = soup.find_all('div', class_='mt-xs-3 clearfix')
+    for link in links_soup:
+        try:
+            link = link.find('a').get('href')
+            links.append('https://www.etsy.com' + link)
+        except:
+            print('  No link')
+
+    images_soup = soup.find_all('div', class_='card-img-wrap')
+    for image in images_soup:
+        try:
+            image = image.find('img').get('src')
+            images.append(image)
+        except:
+            print('  No image')
+
+# Далее записываем данные в файл .xls
+book = xlwt.Workbook('utf8')  # Создаем книгу
+# Создаем шрифт
+font = xlwt.easyxf('font: height 240,name Arial,colour_index black, bold off,\
+    italic off; align: wrap on, vert top, horiz left;\
+    pattern: pattern solid, fore_colour white;')
+# Добавляем лист
+sheet = book.add_sheet('NikkiPattern')
+# Заполняем ячейки (Строка, Колонка, Текст, Шрифт)
+m = 0
+for i in range(len(names)):
+    sheet.write(m, 0, names[i], font)
+    sheet.write(m, 1, links[i], font)
+    sheet.write(m, 2, images[i], font)
+    m = m + 1
+
+sheet.row(1).height = 2500  # Высота строки
+sheet.col(0).width = 20000  # Ширина колонки
+sheet.col(1).width = 20000
+sheet.col(2).width = 20000
+
+sheet.portrait = False  # Лист в положении "альбом"
+sheet.set_print_scaling(85)  # Масштабирование при печати
+book.save('Shop1_NikkiPattern.xls')  # Сохраняем в файл
